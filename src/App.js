35794a52fb10b052cc985b192273ea8db4e8bf9d
Carol Wargo <Carolwargo@gmail.com>
2023-10-23 10:00:21 -0400

    added installs and imports for styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./views/Home/Home";
 
 import "./App.css";
 
 function App() {
   return (
+    <BrowserRouter>
+    <Routes>
     <div className="App">
       <Navbar />
       <Header />
+      <Route path="/" element={<Home />} />
     </div>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+  MDBRipple
+} from 'mdb-react-ui-kit';
+
+
+export default function App() {
+  return (
+    <MDBCard>
+        <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+            <MDBCardImage src='https://C:\Users\carol\OneDrive\Desktop\two-faced\src\assets\MeCard.png' fluid alt='...' />
+            {/*<a href='#'>
+                <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+            </a>
+  */}
+        </MDBRipple>
+        <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+            </MDBCardText>
+            <MDBBtn href='#'>Button</MDBBtn>
+        </MDBCardBody>
+    </MDBCard>
+  );
+}
