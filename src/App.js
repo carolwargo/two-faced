35b932a79b9204cbc9352f9efd9489d0deeae0fa
Carol Wargo <Carolwargo@gmail.com>
2023-11-03 09:27:48 -0400

    added images & icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
-
 import Footer from "./components/Footer/index";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
